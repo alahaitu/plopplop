36a96272403a7bff12baf9e04a0d9d4dbcced40f
Tuomas Alahäivälä <tuomas.alahaivala@leadin.fi>
2018-10-13 19:07:46 +0300

    Mato prefab and stuff

---
+++ Assets/Scripts/SpinZ.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinZ : MonoBehaviour {
+
+    public float speed = 2f;
+    
+    void Update ()
+    {
+        transform.Rotate(new Vector3 (transform.localPosition.x, transform.localPosition.y, transform.localPosition.y) * (speed * Time.deltaTime));
+    }
+}
